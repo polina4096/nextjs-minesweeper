c11534ced0302c0f9d15343fbda3b9a1c298824f
polina4096 <33405371+polina4096@users.noreply.github.com>
2023-05-15 12:43:34 +0300

    Initial game implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,176 @@
-import Image from 'next/image'
+'use client';
+
+import { useState, MouseEvent } from 'react'
+import './board.css';
+import _ from 'lodash';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const boardSize = 9;
+  const mineDensity = 0.1;
+
+  // Current board state:
+  //  -2 => exploded
+  //  -1 => unrevealed
+  // >=0 => neighbor mines
+  const [board, setBoard] = useState(
+    Array.from({ length: boardSize * boardSize }, () => -1)
+  );
+
+  // Mine placement:
+  // true  => mine
+  // false => clear
+  const [mines, setMines] = useState(
+    Array.from({ length: boardSize * boardSize }, () => Math.random() > mineDensity ? false : true)
+  );
+
+  const [revealed, setRevealed] = useState(
+    new Set()
+  );
+
+  function inbounds(x: number, y: number, size: number) {
+    return x >= 0 && x < size && y >= 0 && y < size;
+  }
+
+  function revealCell(idx: number) {
+    let neighbors = calculateNeighbors(idx);
+    board[idx] = neighbors;
+
+    function revealSide(x: number, y: number) {
+      let idx = x + y * boardSize;
+      if (!revealed.has(idx) && inbounds(x, y, boardSize)) {
+        revealed.add(idx);
+  
+        let neighbors = calculateNeighbors(idx);
+        if (neighbors == 0)
+          revealCell(idx);
+        else
+          board[idx] = neighbors;
+      }
+    }
+
+    if (neighbors == 0) {
+      let x = idx % boardSize;
+      let y = Math.floor(idx / boardSize);
+
+      // Cross
+      revealSide(x - 1, y);
+      revealSide(x + 1, y);
+      revealSide(x, y - 1);
+      revealSide(x, y + 1);
+
+      // Diagonal
+      revealSide(x + 1, y + 1);
+      revealSide(x + 1, y - 1);
+      revealSide(x - 1, y + 1);
+      revealSide(x - 1, y - 1);
+    }
+  }
+
+  function onCellRightClick(e: MouseEvent, idx: number) {
+    e.preventDefault();
+
+    if (board[idx] == -1) {
+      board[idx] = -3;
+      setBoard([...board]);
+    } else if (board[idx] == -3) {
+      board[idx] = -1;
+      setBoard([...board]);
+    }
+  }
+
+  function onCellClick(idx: number) {
+    if (board[idx] == -3) return;
+
+    if (mines[idx]) {
+      board[idx] = -2;
+      setBoard([...board]);
+    } else {
+      revealCell(idx);
+      setBoard([...board]);
+    }
+  }
+
+  function calculateNeighbors(idx: number) {
+    let x = idx % boardSize;
+    let y = Math.floor(idx / boardSize);
+
+    let neighborMines = 0;
+
+    function mineAt(x: number, y: number): boolean {
+      return inbounds(x, y, boardSize) && mines[x + y * boardSize];
+    }
+
+    // Mid
+    if (mineAt(x    , y)) neighborMines++;
+    if (mineAt(x + 1, y)) neighborMines++;
+    if (mineAt(x - 1, y)) neighborMines++;
+    
+    // Top
+    if (mineAt(x    , y - 1)) neighborMines++;
+    if (mineAt(x + 1, y - 1)) neighborMines++;
+    if (mineAt(x - 1, y - 1)) neighborMines++;
+    
+    // Bottom
+    if (mineAt(x    , y + 1)) neighborMines++;
+    if (mineAt(x + 1, y + 1)) neighborMines++;
+    if (mineAt(x - 1, y + 1)) neighborMines++;
+
+    return neighborMines;
+  }
+
+  let gridElement = _.chunk(board, boardSize).map((cells, i) =>
+    <div key={i} className='flex flex-row'>
+      {
+        cells.map((cell, j) => {
+          let cellClass;
+
+          switch(board[i + j * boardSize]) {
+            // Flag
+            case -3: {
+              cellClass = 'bg-slate-400 flag';
+
+              break;
+            }
+
+            // Explosion
+            case -2: {
+              cellClass = 'bg-rose-200';
+
+              break;
+            }
+
+            // Unrevealed
+            case -1: {
+              cellClass = 'bg-slate-300';
+
+              break;
+            }
+
+            // Clear
+            default: {
+              cellClass = 'bg-slate-500';
+
+              break;
+            }
+          }
+          
+          return <div
+            key={i + j * boardSize}
+            onContextMenu={(e) => onCellRightClick(e, i + j * boardSize)}
+            onClick={() => onCellClick(i + j * boardSize)}
+            className={`${cellClass} cell text-slate-200 shadow-md shadow-slate-500/25 rounded transition hover:scale-110 cursor-pointer flex items-center justify-center`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            {board[i + j * boardSize] > 0 && board[i + j * boardSize]}
+          </div>;
+        })
+      }
+    </div>
+  );
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  return (
+    <main className='w-full h-full flex flex-col justify-center items-center'>
+      <div className='board'>
+        {gridElement}
       </div>
     </main>
   )
