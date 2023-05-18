61d9709ac8f185750f0a33e756c13acceac25be6
polina4096 <33405371+polina4096@users.noreply.github.com>
2023-05-18 18:50:12 +0300

    Game restarts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,13 @@ export default function Home() {
     mineDensity: 0.125,
   });
 
+  const [gameProps, setGameProps] = useState({...config });
+  const [gameKey, setGameKey] = useState(0);
+
   return <>
     <main className='w-full h-full flex flex-col justify-center items-center'>
       <div className='board'>
-        <Board />
+        <Board key={gameKey} {...gameProps} />
       </div>
     </main>
     <Menu>
@@ -29,7 +32,15 @@ export default function Home() {
             }
           });
         }} />
-        <button className="px-4 py-2 font-semibold text-sm bg-sky-400 dark:bg-sky-500 text-white rounded-full tracking-widest px-6 shadow-sm transition hover:scale-110">New Game</button>
+        <button
+          onClick={() => {
+            setGameProps({...config });
+            setGameKey(gameKey + 1);
+          }}
+          className="px-4 py-2 font-semibold text-sm bg-sky-400 dark:bg-sky-500 text-white rounded-full tracking-widest px-6 shadow-sm transition hover:scale-110"
+        >
+          New Game
+        </button>
       </div>
     </Menu>
   </>;
